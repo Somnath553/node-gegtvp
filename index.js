cfe8f7fae435c703f367d67fe247bc454a1a320e
Somnath553 <73129773+Somnath553@users.noreply.github.com>
2023-03-31 12:22:35 +0530

    1 file modified

--- index.js
+++ index.js
@@ -17,8 +17,36 @@ app.post('/bfhl', urlencodedParser,(req,res)=>{
   let oddnum=[]
   let evennum=[]
   let alphabet=[]
+
+  for(let i in arr)
+  {
+    if(isinfinite(arr[i]))
+    {
+       if(arr[i]%2==0)
+       {
+         evennum.push(arr[i])
+       }
+       ele{
+         oddnum.push(arr[i])
+       }
+    }
+    else
+    {
+      alphabet.push(arr[i])
+    }
+  }
+
+  cont responce={
+    is_success: true,
+user_id: "john_doe_17091999",
+ email : 'john@xyz.com',
+roll_number:'ABCD123',
+odd_numbers:evennum,
+even_numbers:oddnum,
+alphabets:alphabet
+  }
   
-  res.send(arr)
+  res.send(responce)
 
 })
 app.get('/',(req,res)=>{
